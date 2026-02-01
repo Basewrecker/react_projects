ccc887378035c79616d7dc3bc5a66c1eec764cba
Basewrecker <siddhunrs@gmail.com>
2026-02-01 19:32:39 +0530

    you can now remove notes

--- notes-app/src/App.jsx
+++ notes-app/src/App.jsx
@@ -7,12 +7,18 @@ const App = () => {
     
     const[notes, setNotes] = useState([]);
     
+    const deleteNote = (id) => {
+        setNotes(notes.filter((note) => note.id !== id));
+    }
+    
+    
+    
     return <div className = "max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
         <h2 className = "text-2xl font-bold mb-4 text-center">
             Notes app
         </h2>
         <NoteForm notes  = {notes} setNotes = {setNotes}/>
-        <NoteList notes = {notes}/>
+        <NoteList notes = {notes} deleteNote = {deleteNote}/>
     </div>
 };
 
