5ca1e931c3efe75749067eb0457aaa94428d6a1e
Basewrecker <siddhunrs@gmail.com>
2026-02-04 19:50:49 +0530

    added time to local storage via usef

--- timer/src/components/Button.jsx
+++ timer/src/components/Button.jsx
@@ -1,8 +1,19 @@
+import {useRef, useEffect} from "react";
+
 const Button = ({isRunning, onToggle, onReset}) => {
+    
+    const startButtonRef = useRef(null);
+    
+    useEffect(() => {
+        if (startButtonRef.current) {
+            startButtonRef.current.focus();
+        }
+    }, [])
+    
     return (
         
         <>
-        <button onClick = {onToggle} className = "mt-5 bg-green-500 px-4 py-2 rounded-lg hover:-translate-y-1 transition-all duration-200 text-white font-semibold hover:cursor-pointer" id = "start-btn">
+        <button tabindex = {0} ref = {startButtonRef} onClick = {onToggle} className = "mt-5 bg-green-500 px-4 py-2 rounded-lg hover:-translate-y-1 transition-all duration-200 text-white font-semibold hover:cursor-pointer" id = "start-btn">
             {isRunning ? 'Pause' : 'Start'} 
         </button>
         <button onClick = {onReset} className = "ml-10 bg-red-400 px-4 py-2 rounded-lg text-white font-semibold hover:-translate-y-1 transition-all duration-200 hover:cursor-pointer">
--- timer/src/components/Timer.jsx
+++ timer/src/components/Timer.jsx
@@ -1,4 +1,4 @@
-import {useState, useRef} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import TimerDisplay from "./TimerDisplay.jsx";
 import Button from "./Button.jsx";
 
@@ -6,8 +6,15 @@ import Button from "./Button.jsx";
 const Timer = () => {
     const timerRef = useRef(null);
     
-    const [time, setTime] = useState(0);
+    
+    const [time, setTime] = useState(() => {
+        return Number(localStorage.getItem('time') || 0)
+    });
     const [isRunning, setIsRunning] = useState(false);
+    
+    useEffect(() => {
+        localStorage.setItem('time', time);
+    }, [time])
     
     const toggleTimer = () => {
         if (isRunning) {
@@ -29,6 +36,7 @@ const Timer = () => {
             setIsRunning(false);
             timerRef.current = null;
             setTime(0);
+            localStorage.removeItem('time');
             const startBtn = document.getElementById('start-btn');
             startBtn.innerHTML = 'Start'
         }
