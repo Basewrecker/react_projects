163f7f9df3f0e60ea366cd263d7d86f3a090e332
Basewrecker <siddhunrs@gmail.com>
2026-02-06 10:38:37 +0530

    api fetching being displayed in dom

--- crypto_v1_ffp/src/App.jsx
+++ crypto_v1_ffp/src/App.jsx
@@ -27,8 +27,41 @@ const App = () => {
     return (
     <div>
         <h1>🚀 Crypto</h1>
-    </div>
+        {loading && <p>Loading</p>}
+        {error && <div className = "error">{error} </div> }
     
+        
+    {!loading && !error && (
+     <main className = "grid">
+         {coins.map((coin) => (
+         <div className = "coin-card" key = {coin.id}>
+             <div className = "coin-header">
+                 <img src={coin.image} alt={coin.name} class = "coin-image"/>
+                 <div>
+                     <h2>
+                         {coin.name}
+                     </h2>
+                     <p className = "symbol">
+                         {coin.symbol.toUpperCase()}
+                     </p>
+                 </div>
+             </div>
+             <p>
+               Price: ${coin.current_price.toLocaleString()}  
+             </p>
+             <p className = {coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}>
+                 {coin.price_change_percentage_24h.toFixed(2)}%
+             </p>
+             <p>
+                Market cap: {coin.market_cap.toLocaleString()}
+             </p>
+         </div>
+         
+         ))}
+     </main>
+     )}
+    
+    </div>
     );
 }
 
