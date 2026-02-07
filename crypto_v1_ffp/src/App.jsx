b64e93d684418b71bee6e6c303cb3a64ced433b9
Basewrecker <siddhunrs@gmail.com>
2026-02-07 18:27:47 +0530

    added filter field text

--- crypto_v1_ffp/src/App.jsx
+++ crypto_v1_ffp/src/App.jsx
@@ -1,12 +1,15 @@
 import {useState, useEffect} from "react";
 import CoinCard from "./components/CoinCard.jsx"
+import LimitSelector from "./components/LimitSelector.jsx"
+import FilterInput from "./components/FilterInput.jsx"
 
 const App = () => {
     
     const [coins, setCoins] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [limit, setLimit] = useState(20);
+    const [limit, setLimit] = useState(50);
+    const [filter, setFilter] = useState('');
     
     useEffect(() => {
         fetch(`/api/coins?limit=${limit}`)
@@ -26,32 +29,30 @@ const App = () => {
         })
     }, [limit]);
     
+    const filteredCoins = coins.filter((coin) => {
+        return coin.name.toLowerCase().includes(filter.toLocaleLowerCase())
+    });
+    
     return (
     <div>
         <h1>🚀 Crypto</h1>
         {loading && <p>Loading</p>}
         {error && <div className = "error">{error} </div> }
         
-        <div className="controls">
-            <label htmlFor="limit">
-                Show:
-            </label>
-            <select id="limit" value = {limit} onChange = {(event) => setLimit(Number(event.target.value))}>
-               <option value="5">5</option>
-               <option value="10">10</option>
-               <option value="20">20</option>
-               <option value="50">50</option>
-               <option value="100">100</option>
-            </select>
+        <div className="top-controls">
+            <FilterInput filter = {filter} onFilterChange = {setFilter}/>
+            <LimitSelector limit = {limit} onLimitChange = {setLimit}    />
         </div>
-    
+        
+            
+        
         
     {!loading && !error && (
      <main className = "grid">
          {coins.map((coin) => (
         
           <CoinCard key = {coin.id} coin = {coin} />
-         ))}
+         ))} 
      </main>
      )}
     
