cd1c19ac807a00b0b6132c3a2fb23cfa2785db3c
Basewrecker <siddhunrs@gmail.com>
2026-02-09 20:52:57 +0530

    replaced a with link tag

--- crypto_v1_ffp/src/components/Header.jsx
+++ crypto_v1_ffp/src/components/Header.jsx
@@ -1,8 +1,10 @@
+import {Link} from "react-router";
+
 const Header = () => {
     return (
       <div className = "top-nav">
-          <a href="/">Home</a>
-          <a href="/about">About</a>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
       </div>
     )
 }
