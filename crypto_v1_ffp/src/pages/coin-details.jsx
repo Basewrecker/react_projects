84be3f18171df662372c00b870e6d2fdda217eb6
Basewrecker <siddhunrs@gmail.com>
2026-02-10 20:13:19 +0530

    shows cur price and basic info

--- crypto_v1_ffp/src/pages/coin-details.jsx
+++ crypto_v1_ffp/src/pages/coin-details.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react";
-import {useParams} from "react-router";
+import {useParams, Link} from "react-router";
 const API_URL = import.meta.env.VITE_COIN_API_URL;
 
 const CoinDetailsPage = () => {
@@ -15,7 +15,7 @@ const CoinDetailsPage = () => {
                 const res = await fetch(`${API_URL}/${id}`);
                 if (!res.ok) throw new Error('Failed to fetch the data');
                 const data = await res.json();
-                console.log(data);
+                console.log('API_URL', API_URL);
                 setCoin(data);
             } catch (error) {
                 
@@ -30,8 +30,32 @@ const CoinDetailsPage = () => {
     
     
     return (
-      <div>
-          Coin Details: {id}
+      <div className = "coin-details-container">
+          <Link to = "/">--- Back to home</Link>
+          
+          <h1 className = "coin-details-title">
+              {coin ? `${coin.name} (${coin.symbol})` : 'Coin Details'}
+          </h1>
+          
+          {loading && <p>Loading...</p>}
+          {error && <div className = "error">{`❌ ${error}`}</div>}
+          
+            {!loading && !error && (
+              <>
+                <img src={coin.image.large} alt={coin.name} className = "coin-details-image"/>
+                <p>
+                    {coin.description.en.split('. ')[0] + '.'}
+                </p>
+                <div className = "coin-details-info">
+                    <h3>
+                        Rank: #{coin.market_cap_rank}
+                    </h3>
+                    <h3>
+                        Current Price: {coin.market_data.current_price.usd}
+                    </h3>
+                </div>
+              </>
+            )}
       </div>
     )
 }
