d7350db53db1d58717b43b9b5953bc90dbad5592
Basewrecker <siddhunrs@gmail.com>
2026-02-15 17:02:35 +0530

    added context api to absorb the products json

---
+++ shopping-ui/src/context/ProductContext.jsx
@@ -0,0 +1,37 @@
+import {createContext, useState, useEffect, useContext} from "react";
+
+export const ProductContext = createContext();
+
+export function ProductProvider({children}) {
+    const [products, setProdcuts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await fetch('/api/products')
+                if (!res.ok) throw new Error('Failed to fetch data')
+                const data = await res.json();
+                setProdcuts(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        
+        fetchProducts();
+    }, [])
+    
+    return (
+     <ProductContext.Provider value = {{products, loading, error}}>
+       {children}
+     </ProductContext.Provider>
+    
+    )
+}
+
+export function useProducts() {
+    return useContext(ProductContext)
+}
